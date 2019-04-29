b34c934e1f6f3deafd8d3c037261853f4c2914c9
Carter Carlson <carlfarterson@gmail.com>
2019-04-29 12:44:14 -0500

    Finish chrome class

--- chrome.py
+++ chrome.py
@@ -1,9 +1,20 @@
+import os
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
 
+class Chrome:
+    def __init__(self):
+        self._connect()
 
-def connect(url):
-    pass
+    def _connect(self):
+        self.web = webdriver.Chrome(os.getcwd() + '/chromedriver', options=self.options)
+
+    def refresh(self):
+        self.web.close()
+        self._connect()
+
+    def get(url):
+        self.web.get(url)

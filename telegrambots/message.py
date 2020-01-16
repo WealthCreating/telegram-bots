d9d00db745b0142232f65f9860bafb41ce70d44c
Carter Carlson <carlfarterson@gmail.com>
2020-01-16 16:24:20 -0600

    Move python files to correct sub-directories

--- telegrambots/message.py
+++ telegrambots/message.py
@@ -3,11 +3,11 @@ import importlib
 from urllib.parse import quote_plus
 
 
-def send_message(file, message):
+def message(file, kwargs):
     filename = file.split('/')[-1]
     botname = filename[:filename.find('.')]
     api = importlib.import_module('telegram.api.' + botname)
     msg = 'https://api.telegram.org/bot' + api.key()
     msg += '/sendMessage?chat_id=' + api.chat_id()
-    msg += '&text=' + quote_plus(message)
+    msg += '&text=' + quote_plus(kwargs)
     requests.get(msg)

cdc7c94bf2a6d1fbcfdf94e5465498598f7945ea
Carter Carlson <carlfarterson@gmail.com>
2020-01-16 20:24:45 -0600

    Write untested version of Bot

--- telegrambots/bot.py
+++ telegrambots/bot.py
@@ -1,11 +1,43 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 import pandas as pd
 import numpy as np
 
+import os
+from api import token
+
 
 class Bot:
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, token, *args, **kwargs):
+        self.token = token
+        # Create the updater and pass in token
+        self.updater = Updater(token, use_context=True)
+        # Get dispatcher to register handlers
+        self.dp = self.updater.dispatcher
+        # On different command - answer in Telegram
+        self.dp.add_handler(CommandHandler('start', self.start))
+
+        # Log all errors
+        self.dp.add_error_handler(self.error_handler)
+
+        # Start the bot
+        self.updater.start_polling()
+
+        self.updater.idle()
+
+
+    def error_handler(self, update, context):
         pass
 
-    # 1. Send message to user
+
+    def start(self, update, context):
+        self.update.message.reply_text('Test response')
+
+
+    # Send message to user
     def message(self, username, text):
         pass
+
+
+b = Bot(token)
--- telegrambots/extra.py
+++ telegrambots/extra.py
@@ -2,11 +2,17 @@
 
 # 2020.01.16
 # Run something similar to echobot2.py from python-telegram-bot
-
-import telegram
-from api import key
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from api import token
 
 
 bot = telegram.Bot(token=token)
 
 print(bot.get_me())
+# Create the Updater and pass the bots' toen
+updater = Updater(token, use_context=True)
+
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler('start', start))

884f84b8d59928b4a6a9192ff53c86e365964b7d
Carter Carlson <carlfarterson@gmail.com>
2020-01-18 11:06:31 -0600

    Add sufficient Telebot example

--- telegrambots/bot-telebot.py
+++ telegrambots/bot-telebot.py
@@ -1,10 +1,15 @@
 from api import token
-
 import telebot
+import pandas as pd
 
-import telegram.ext
+users = pd.read_csv('../data/users.csv')
 
+
+# Create bot
 bot = telebot.TeleBot(token)
+
+
+
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -15,27 +20,19 @@ def start(message):
     print(message.text)
     bot.reply_to(message, "You're a wizard, Carter!")
 
-# Experimental
-# @bot.message_handler(lambda query: '@' in query.text)
-@bot.message_handler(func=lambda message: '@' in message.text)
+def validate(message):
+    users = pd.read_csv('../data/users.csv')
+    # TODO: validate username in users
+
+@bot.message_handler(func=lambda m: '@' in m.text)
 def handle_email(message):
-    print(f'This is your email: {message.text}')
-    print(message)
+    print(type(message))
+    validate(message)
     bot.reply_to(message, f'This is your email: {message.text}')
 
-# @bot.message_handler(func=lambda message: True)
-# def handle_message(message):
-#     if '@' in message.text:
-#         # Show `SUCCESS`
-#         print('GREAT SUCCESS')
-#         bot.reply_to(message, 'GREAT SUCCESS')
-#     else:
-#         pass
 
+
+
+# TODO: add buttons
 
 bot.polling()
-
-
-message
-
-# TODO:  Add buttons to telebot

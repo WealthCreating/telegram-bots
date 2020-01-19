379a266abefe8e93facf83195372a277add8328d
Carter Carlson <carlfarterson@gmail.com>
2020-01-19 09:09:09 -0600

    Add code to main.py to work with current bot_class.py file

--- telegrambots/main.py
+++ telegrambots/main.py
@@ -1,25 +1,11 @@
+# Running the bot
+from bot_class import bot
+
 from datetime import datetime, timedelta
 from time import sleep
-import chrome
-from bots import btc_candle, tether
 
 
 if __name__ == '__main__':
 
-    chrome = chrome.Chrome()
-    for i in range(1, 10**10):
-        sleep(1)
-        # Restart browser every hour
-        if i % 3600 == 0:
-            chrome.refresh()
-
-        # Run tether bot every 2 min
-        if i % 120 == 0:
-            tether.run(chrome)
-
-        # Run btc_candle bot at 23:59:00 UTC
-        now = datetime.utcnow()
-        if now.hour == 23 and now.minute > 58:
-            btc_candle.run(now)
-            sleep(60)
-            btc_candle.run(now + timedelta(seconds=60))
+    bot = Bot()
+    bot.start()
